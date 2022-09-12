0a243371a25280383dbae087fa05f120dbe1a764
Grigory <gregory.rvl@yandex.ru>
2022-09-12 18:28:18 +0300

    fixing lint and nove for appveyor

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,24 @@ import Magician from './magician';
 import Daemon from './daemon';
 import Undead from './undead';
 import Zombie from './zombie';
+
+const warrior = new Character('warrior', 'Swordsman', 100, 1, 20, 20);
+console.log(warrior);
+
+const bowman = new Bowman('bowman');
+console.log(bowman);
+
+const sword = new Swordsman('sword');
+console.log(sword);
+
+const magicman = new Magician('magicman');
+console.log(magicman);
+
+const ghoul = new Undead('ghoul');
+console.log(ghoul);
+
+const notdead = new Daemon('notdead');
+console.log(notdead);
+
+const brainz = new Zombie('brainz');
+console.log(brainz);
